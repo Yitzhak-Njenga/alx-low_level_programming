5e18726a39db829b8fab397edefbb3c25bf25d15
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2022-03-12 15:23:44 +0300

    rabdom nnumber

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,3 +1,5 @@
+
+  
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,19 +10,20 @@
  */
 int main(void)
 {
-	int d;
+	int n;
+	int L;
+	char str[] = "Last digit of";
 
-	for (d = 0; d < 100; d++)
-	{
-		putchar((d / 10) + '0');
-		putchar((d % 10) + '0');
-		if (d != 99)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	L = n % 10;
+
+	if (L > 5)
+		printf("%s %d is %d and is greater than 5\n", str, n, L);
+	else if (L == 0)
+		printf("%s %d is %d and is 0\n", str, n, L);
+	else if (L < 6)
+		printf("%s %d is %d and is less than 6 and not 0\n", str, n, L);
 
 	return (0);
 }

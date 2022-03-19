4e4f90a34b956f30a3583017da3dfa12a69bf133
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2022-03-19 17:19:37 +0300

    putcher a

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*
+*void print_most_numbers
+*
+*
+*
+*/
+
+void print_most_numbers(void)
+{
+int i;
+        for (i = 0; i < 9; i++)
+        {
+            if (i == 2 || i == 4)
+            {
+                continue;
+            }
+
+            putchar(i);
+
+        }
+        putchar('\n');
+        
+}
